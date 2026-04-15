6fef3b9cac4e8a9573ed47e7d9d8125c1f0664e9
Ron69 <98207233+Ron0609@users.noreply.github.com>
2026-04-15 11:25:14 +0900

    Change server to listen on dynamic PORT
    Updated server to use dynamic port from environment variable.

--- app/server.js
+++ app/server.js
@@ -5,6 +5,7 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 const prefix = '.';
 
 const selectedMembers = {}; // 用于保存选中的成员，按照频道 ID 存储
+const PORT = process.env.PORT || 3000;
 
 http.createServer(function(req, res){
  if (req.method == 'POST'){
@@ -32,9 +33,9 @@ http.createServer(function(req, res){
    res.writeHead(200, {'Content-Type': 'text/plain'});
    res.end('Discord Bot is active now\n');
  }
-}).listen(3000);
-
-
+}).listen(PORT, () => {
+  console.log("HTTP server running on port " + PORT);
+});
 
 client.once('ready', () => {
     console.log('Bot準備完了～');
