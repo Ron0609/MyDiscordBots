fe2985ed53110831d04cd8ae8e35d63b099ebb44
Ron69 <98207233+Ron0609@users.noreply.github.com>
2026-04-15 10:01:56 +0900

    Configure server to use environment port
    Updated server to listen on a configurable port and added logging.

--- app/server.js
+++ app/server.js
@@ -32,8 +32,14 @@ http.createServer(function(req, res){
    res.writeHead(200, {'Content-Type': 'text/plain'});
    res.end('Discord Bot is active now\n');
  }
-}).listen(3000);
+});
 
+// Render
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("HTTP server running on port " + PORT);
+});
 
 
 client.once('ready', () => {
