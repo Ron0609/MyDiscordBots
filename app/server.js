7cd71e4af6985df67ade4668a0e5929854f36993
Ron69 <98207233+Ron0609@users.noreply.github.com>
2026-04-15 11:31:44 +0900

    Add GUILD_VOICE_STATES intent to Discord client

--- app/server.js
+++ app/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const querystring = require('querystring');
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
 const prefix = '.';
 
 const selectedMembers = {}; // 用于保存选中的成员，按照频道 ID 存储
